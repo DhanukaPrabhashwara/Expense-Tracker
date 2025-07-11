6016ceab031de35356f89dc9f584e8ce26eb1065
DhanukaPrabhashwara <IT22193704@my.sliit.lk>
2025-07-11 20:54:13 +0530

    image upload is now working

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const { connect } = require("http2");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -15,6 +18,13 @@ app.use(
 );
 
 app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+//Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
