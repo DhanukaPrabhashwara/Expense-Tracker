e9994a6c4bca1e87fb3abfa85e819d7cdc0bc841
DhanukaPrabhashwara <IT22193704@my.sliit.lk>
2025-12-16 09:04:22 +0530

    Styles update

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ExpenseSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     icon: { type: String },
-    category: { type: String, required: true }, //ex- food, rent
+    category: { type: String, required: true },
     amount: { type: Number, required: true },
     date: { type: Date, default: Date.now },
 }, { timestamps: true });
